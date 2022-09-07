82a6a73bb5be14fb928acd1cd838dacce74350fc
Dariykaa <111238992+Dariykaa@users.noreply.github.com>
2022-09-07 13:22:57 +0300

    Create 3.js

---
+++ 3.js
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String str = "123";
+        int n = Integer.parseInt(str);
+
+        if((n%2)==0){
+            System.out.println();
+        }
+        else{
+            System.out.println(n);
+            }
+
+
+
+    }
+}
