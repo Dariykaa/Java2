41126237d8deb862b058cbfd3246a2df89c62209
Dariykaa <111238992+Dariykaa@users.noreply.github.com>
2022-09-07 10:26:24 +0300

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int g = 4;
+        int h = 3;
+        boolean t = g > h;
+        String simpleString = "This is a regular string. Just as standard and unremarkable as the rest of us.";
+        int a = 0b1100101011;
+        int b = 0123145;
+        int c = 6890;
+        int d = 0x141F12;
+        float f = 4.05E-13F;
+        double i = 2.718281828459045;
+
+        System.out.println(t);
+        System.out.println(simpleString);
+        System.out.println("Question Mark - " + '\u003F');
+        System.out.println("Number a in binary system: " + a);
+        System.out.println("Number b in octal system: " + b);
+        System.out.println("Number c in decimal: " + c);
+        System.out.println("Number d in hexadecimal system: " + d);
+        System.out.println("float type in the form: " + f);
+        System.out.println("double type in the form: " + i);
+
+
+
+
+    }
+}
