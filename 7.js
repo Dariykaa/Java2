0d5994c0ea802d16d5a0be7ec30a8386683e3f70
Dariykaa <111238992+Dariykaa@users.noreply.github.com>
2022-09-07 18:53:46 +0300

    Create 7.js

---
+++ 7.js
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 1;
+        while(number <= 10) {
+            System.out.println("Task" + number);
+            number++;
+        }
+    }
+}
